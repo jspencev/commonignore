99f506521ec8aabeef52afc0a9ccbd73ee168619
John Spence <john.spence.v@gmail.com>
2020-10-19 17:07:10 -0700

    feat: 🎸 add --add -a flag that runs git add [filenames]

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,27 @@
 import commonignore from './api/commonignore';
+import { spawnChildProcess } from './util';
 
 (async function() {
+  const argv = require('yargs')
+    .option('add', {
+      alias: 'a',
+      description: 'Run git add [filenames] after the files are created.'
+    })
+    .help()
+    .argv;
+
   const files = await commonignore(process.cwd());
-  let done = 'commonignore: Wrote ';
+  let writeConfirmation = 'commonignore: Wrote ';
+  const gitAdd = ['add'];
   for (const filename in files) {
-    done += `${filename}, `;
+    writeConfirmation += `${filename}, `;
+    gitAdd.push(filename);
   }
-  done = done.slice(0, -2);
-  console.log(done);
+  writeConfirmation = writeConfirmation.slice(0, -2);
+  console.log(writeConfirmation);
+
+  if (argv.add) {
+    console.log(`git ${gitAdd.join(' ')}`);
+    await spawnChildProcess('git', gitAdd);
+  }
 })();
